3ce9a978ecfbd972d192d5b95c40d7feb07f64be
Neal Xu <imxuneal@gmail.com>
2020-07-06 11:19:25 +0800

    Init project

---
+++ src/demo.ts
@@ -0,0 +1,10 @@
+
+const btn = document.createElement('button');
+btn.innerText = "I'm imKey Connect";
+btn.addEventListener('click', (e) => {
+
+});
+
+
+// document.appendChild(btn);
+document.body.append(btn);
